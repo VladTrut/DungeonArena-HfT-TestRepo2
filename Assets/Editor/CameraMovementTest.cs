184402431d8c719bac0ee02df38f0a5616daed1a
EmulatedUser <32gede1bif@hft-stuttgart.de>
2016-04-24 09:43:15 +0200

    Addition of Camera Movement
    Movement of Camera per Room instead of following the Player

---
+++ Assets/Editor/CameraMovementTest.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using NUnit.Framework;
+
+public class CameraMovementTest {
+
+	CameraMovement cameraMovement = new CameraMovement();
+
+	[Test]
+	public void TestIfCameraCanReachDestination(){
+
+		//initialization
+		Vector3 cameraOriginPosition = new Vector3 (0, 0, 0);
+		Vector3 someOtherPosition = new Vector3 (10, 10, 0);
+
+		//testing
+
+		//1. Destination reached
+		cameraMovement.transform.position = cameraOriginPosition;
+		Assert.That (cameraMovement.CheckIfDestinationIsReached ());
+
+		//2. Destination not reached
+		cameraMovement.changeDestination(someOtherPosition);
+		Assert.That (!cameraMovement.CheckIfDestinationIsReached ());
+
+	}
+
+}
---
+++ Assets/Editor/PlayerEnterDetectorTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using NUnit.Framework;
+
+public class PlayerEnterDetectorTest {
+
+	//private PlayerEnterDetector playerEnterDetector = new PlayerEnterDetector();
+
+	[Test]
+	private void AssertCollisionObjectIsCameraTarget(){
+		
+		////setup
+		//GameObject notCameraRelevant = new GameObject ();
+		//notCameraRelevant.tag = "someOtherTag";
+		//notCameraRelevant.AddComponent (new BoxCollider2D ());
+		//GameObject cameraRelevant = new GameObject ();
+		//cameraRelevant.tag = "CameraCollisionTarget";
+		//cameraRelevant.AddComponent(new BoxCollider2D());
+
+		////test
+		//Assert.That (playerEnterDetector.CheckIfCollisionObjectIsCameraTarget (cameraRelevant.GetComponent(Collider2D)));
+		//Assert.That (!playerEnterDetector.CheckIfCollisionObjectIsCameraTarget (notCameraRelevant.GetComponent(Collider2D)));
+
+	}
+
+
+}

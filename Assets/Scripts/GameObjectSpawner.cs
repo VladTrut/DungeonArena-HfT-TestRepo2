20da5e62a9622240cd1e25d59a8fdd933ae78b02
VolkanKutlar <32kuvo1bif@hft-stuttgart.de>
2016-04-30 13:58:54 +0200

    Build config auf IOS zurückgesetzt => Joistick Steuerung ist dadurch wieder möglich

---
+++ Assets/Scripts/GameObjectSpawner.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameObjectSpawner : MonoBehaviour {
+
+    public void spawnPlayer(GameObject[] spawnPoints, int spawnPointCount)
+    {
+        if(spawnPoints==null || spawnPoints.Length<=0)
+        {
+            return;
+        }
+        int index = 0;
+        foreach (GameObject current in spawnPoints)
+        {
+            Vector3 spawnPosition = current.transform.position;
+
+            if (IsEmptyPosition(spawnPosition))
+            {
+                transform.position = spawnPosition;
+                break;
+            }
+
+            if (index >= spawnPointCount)
+            {
+                spawnPlayer(spawnPoints, spawnPointCount); //Wiederholen bis Spieler gespawned
+                break;
+            }
+
+            index++;
+        }
+    }
+
+    private bool IsEmptyPosition(Vector3 targetPos)
+    {
+        GameObject[] allMovableThings = GameObject.FindGameObjectsWithTag("Physical"); //returns all game object with tag "Physical"
+        foreach (GameObject current in allMovableThings)
+        {
+            if (current.transform.position == targetPos)
+                return false;
+        }
+        return true;
+    }
+}

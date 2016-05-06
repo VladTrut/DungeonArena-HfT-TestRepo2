77600de5bc24a55ac063e36b91f7362aa42405e2
Marcel Mayer <student@students-MacBook-Air-8.local>
2016-05-06 18:59:13 +0200

    Added arrows for players

---
+++ Assets/Scripts/AppQuit.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+namespace UnityTest
+{
+	public class AppQuit : NetworkBehaviour 
+	{
+		/// <summary>
+		/// Called when the application quits.
+		/// </summary>
+		void OnApplicationQuit()
+		{
+			NetworkMan networkman = gameObject.GetComponent<NetworkMan> ();
+			networkman.DestroyMatch ();
+		}
+	}
+}

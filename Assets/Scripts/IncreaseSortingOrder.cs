433f04a14f3dcf6b78116cb041f8a204ecb28197
Vlad <student@students-MacBook-Air-3.local>
2016-05-10 20:21:43 +0200

    ImProgress_1

---
+++ Assets/Scripts/IncreaseSortingOrder.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IncreaseSortingOrder : MonoBehaviour {
+
+	static int renderOrderOffset = 0;
+
+	void Start () 
+	{
+		renderOrderOffset += 1000;
+
+		foreach (var renderer in GetComponentsInChildren<Renderer>())
+			renderer.sortingOrder += renderOrderOffset;
+	}
+
+}
